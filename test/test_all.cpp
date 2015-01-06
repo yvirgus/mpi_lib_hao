824aa323f51e8d6e19e43cf548592000385a39c6
boruoshihao <boruoshihao@gmail.com>
2015-01-05 21:41:37 -0500

    Change indentation to four.

--- test/test_all.cpp
+++ test/test_all.cpp
@@ -6,20 +6,20 @@ void mpi_fun_test();
 
 int main(int argc, char** argv)
 {
- //MPIInit(argc,argv);
- MPIInitFunnel(argc,argv);
+    //MPIInit(argc,argv);
+    MPIInitFunnel(argc,argv);
 
 #ifdef MPI_HAO
- if(MPIRank()==0) cout<<"Testing mpi_lib_hao mpi version......\n"<<endl;
+    if(MPIRank()==0) cout<<"Testing mpi_lib_hao mpi version......\n"<<endl;
 #else
- if(MPIRank()==0) cout<<"Testing mpi_lib_hao serial version......\n"<<endl;
+    if(MPIRank()==0) cout<<"Testing mpi_lib_hao serial version......\n"<<endl;
 #endif
 
- mpi_fun_test();
+    mpi_fun_test();
 
- MPIBarrier();
- if(MPIRank()==0) cout<<"\n\nIf these is no warning, we have passed all the test!"<<endl;
+    MPIBarrier();
+    if(MPIRank()==0) cout<<"\n\nIf these is no warning, we have passed all the test!"<<endl;
 
- MPIFinalize();
- return 0;
+    MPIFinalize();
+    return 0;
 }
